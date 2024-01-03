bdc56570af4fcd771bdbb298bec922771ad006e6
Do Tu Toan <dotoan22112003@gmail.com>
2024-01-03 10:11:04 +0700

    Remove default demo controller and add test controller

---
+++ WebAPI/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return Ok("Your api has been successfully deployed");
+        }
+    }
+}
