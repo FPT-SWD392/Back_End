5e3a0618278bcc877f0a05ad0447bbc71b07bfd0
Do Tu Toan <dotoan22112003@gmail.com>
2024-03-05 07:42:38 +0700

    Add missing repository (not finished)

---
+++ Repository/Implementation/PurchaseRepository.cs
@@ -0,0 +1,63 @@
+﻿using BusinessObject.SqlObject;
+using DataAccessObject;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation
+{
+    public class PurchaseRepository : IPurchaseRepository
+    {
+        private IGenericDao<Purchase> _dao;
+        private ISqlFluentRepository<Purchase> _sqlFluentRepository;
+        public PurchaseRepository(IDaoFactory daoFactory, ISqlFluentRepository<Purchase> sqlFluentRepository)
+        {
+            _dao = daoFactory.CreateDao<Purchase>();
+            _sqlFluentRepository = sqlFluentRepository;
+        }
+
+        public async Task CreateNewPurchase(Purchase purchase)
+        {
+            await _dao.CreateAsync(purchase);
+        }
+
+        public async Task DeletePurchase(Purchase purchase)
+        {
+            await _dao.DeleteAsync(purchase);
+        }
+
+        public async Task<List<Purchase>> GetAllArtPurchases(int artId)
+        {
+            return await _dao
+                .Where(x => x.ArtId == artId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Purchase>> GetAllUserPurchases(int userId)
+        {
+            return await _dao
+                .Where(x => x.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<Purchase?> GetPurchaseById(int purchaseId)
+        {
+            return await _dao
+                .Where(x => x.PurchaseId == purchaseId)
+                .SingleOrDefaultAsync();
+        }
+
+        public ISqlFluentRepository<Purchase> Query()
+        {
+            return _sqlFluentRepository;
+        }
+
+        public async Task UpdatePurchase(Purchase purchase)
+        {
+            await _dao.UpdateAsync(purchase);
+        }
+    }
+}
--- Repository/Interface/IPurchaseRepository.cs
+++ Repository/Interface/IPurchaseRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject.SqlObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Repository.Interface
 {
     public interface IPurchaseRepository
     {
+        public Task CreateNewPurchase(Purchase purchase);
+        public Task DeletePurchase(Purchase purchase);
+        public Task UpdatePurchase(Purchase purchase);
+        public Task<Purchase?> GetPurchaseById(int purchaseId);
+        public Task<List<Purchase>> GetAllUserPurchases(int userId);
+        public Task<List<Purchase>> GetAllArtPurchases(int artId);
+        public ISqlFluentRepository<Purchase> Query();
     }
 }
