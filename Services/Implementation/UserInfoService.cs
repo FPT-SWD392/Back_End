55283b13854c34ac740853cf16a9bac59395ce59
Son Le <lehoangson2259@gmail.com>
2024-03-10 13:17:35 +0700

    1 UpdateProfile(int id, string fullName, string location, string phoneNumber, string nickName) 2 UpdateProfilePicture(int id, string imgBase64) 3 VerifyOldPassword(int id, string oldPassword) 4 UpdatePassword(int id, string newPassword)
    Đúng thì trả kq 200, không có hoặc sai thông tin trả 400 BadRequest

---
+++ Services/Implementation/UserInfoService.cs
@@ -0,0 +1,132 @@
+﻿using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
+using BusinessObject;
+using BusinessObject.SqlObject;
+using Microsoft.Identity.Client;
+using Repository.Implementation;
+using Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.PasswordHasher;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Services.Implementation
+{
+    public class UserInfoService : IUserInfoService
+    {
+        private readonly IUserInfoRepository _userRepository;
+        private readonly ICreatorInfoRepository _creatorInfoRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        public UserInfoService(IUserInfoRepository userInfoRepository, ICreatorInfoRepository creatorInfoRepository, IPasswordHasher passwordHasher)
+        {
+            _userRepository = userInfoRepository;
+            _creatorInfoRepository = creatorInfoRepository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public  Task<UserInfo?> GetUserByUserId(int userId)
+        {
+            return _userRepository.GetUserById(userId);
+        }
+
+        public async Task<UserInfo?> GetUserByCreatorId(int creatorId)
+        {
+            CreatorInfo creator = await _creatorInfoRepository.GetCreatorInfo(creatorId);
+            return creator.UserInfo;
+        }
+        public async Task UpdateProfile(int id, string fullName, string location, string phoneNumber, string nickName)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserById(id);
+                if (user != null)
+                {
+                    user.FullName = fullName;
+                    user.Location = location;
+                    user.PhoneNumber = phoneNumber;
+                    user.NickName = nickName;
+                    await _userRepository.UpdateUser(user);
+                }
+                else
+                {
+                    throw new Exception("Cannot find user");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task UpdateProfilePicture(int id, string imgBase64)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserById(id);
+                if (user != null)
+                {
+                    user.ProfilePicture = imgBase64;
+                    await _userRepository.UpdateUser(user);
+                }
+                else
+                {
+                    throw new Exception("Cannot find user");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<bool> VerifyOldPassword(int id, string oldPassword)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserById(id);
+                if (user != null)
+                {
+                    if (_passwordHasher.VerifyPassword(oldPassword, user.PasswordHash))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Cannot find user");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task UpdatePassword(int id, string newPassword)
+        {
+            try
+            {
+                var user = await _userRepository.GetUserById(id);
+                if (user != null)
+                {
+                    user.PasswordHash = _passwordHasher.HashPassword(newPassword);
+                    await _userRepository.UpdateUser(user);
+                }
+                else
+                {
+                    throw new Exception("Cannot find user");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using BusinessObject;
+using BusinessObject.SqlObject;
+using JwtTokenAuthorization;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Interface;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Utils.PasswordHasher;
+using WebAPI.Model;
+using Microsoft.AspNetCore.Authorization;
+using Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ITokenHelper _jwtHelper;
+        private readonly IUserInfoService _userInfo;
+        private readonly IPasswordHasher _passwordHasher;
+        public UserController(ITokenHelper jwtHelper,IPasswordHasher passwordHasher, IUserInfoService userInfo)
+        {
+            _jwtHelper = jwtHelper;
+            _passwordHasher = passwordHasher;
+            _userInfo = userInfo;
+        }
+
+        [Authorize]
+        [HttpGet("GetAllInfoAboutUser")]
+        public async Task<IActionResult> GetAllInfoAboutUser()
+        {
+            int userId = Int32.Parse(_jwtHelper.GetUserIdFromToken(HttpContext));
+            var result = await _userInfo.GetUserByUserId(userId);
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpPut("UserManageProfile")]
+        public async Task<IActionResult> ManageProfile([FromQuery] UserProfile user)
+        {
+            try
+            {
+                int userId = Int32.Parse(_jwtHelper.GetUserIdFromToken(HttpContext));
+                await _userInfo.UpdateProfile(userId, user.FullName, user.Location, user.PhoneNumber, user.NickName);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut("UpdateProfilePicture")]
+        public async Task<IActionResult> UpdateProfilePicture(string imgBase64)
+        {
+            try
+            {
+                int userId = Int32.Parse(_jwtHelper.GetUserIdFromToken(HttpContext));
+                await _userInfo.UpdateProfilePicture(userId, imgBase64);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut("UpdateProfilePassword")]
+        public async Task<IActionResult> UpdateProfilePassword(string oldPassword, string newPassword)
+        {
+            try
+            {
+                int userId = Int32.Parse(_jwtHelper.GetUserIdFromToken(HttpContext));
+                var check = await _userInfo.VerifyOldPassword(userId, oldPassword);
+                if (check)
+                {
+                    await _userInfo.UpdatePassword(userId, newPassword);
+                    return Ok();
+                }
+                else return BadRequest("Old password is not correct");
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
